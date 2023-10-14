6edf9635398a0e2ffc1ef5d463e1a742292461b0
Amaranadh9989 <amaranadhmatta123@gmail.com>
2023-10-15 01:28:15 +0530

    Description of changes

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,7 +8,7 @@ const ContactForm = () => {
       <div className="cf" id='form_container'>
       <form action="https://formsubmit.co/683c2aec7b2abc33377c8b4b55e6e404 " method="POST">
         <div className="half left cf">
-          <input type="text" name="Name" className="input-name" placeholder="Name" required/>
+          <input type="text" name="Name" className="input-name" placeholder="Full Name" required/>
           <input type="email" name="email" className="input-email" placeholder="Email address" required/>
           <input type="text" name="text" className="input-subject" placeholder="Subject" required/>
         </div>
